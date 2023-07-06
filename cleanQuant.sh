75a6976383cad4d1f00081f002000d261dc40f83
imoppel1004 <imoppel@oberlin.edu>
2023-07-06 11:44:23 -0400

    changed .fa file to use same format as tximport method

--- cleanQuant.sh
+++ cleanQuant.sh
@@ -1,9 +1,6 @@
 # Isabella Moppel
-# 2023/07/03
+# 2023/07/06
 
-# requires that index is already built, named ref_index
-# otherwise, build index from refSeq.fna by uncommenting the below
-#salmon index -t refSeq.fna -i ref_index
 
 # trim off adapters with scythe
 for i in $(ls *_001.fastq.gz | sed -r 's/_001.fastq.gz//' | uniq)
@@ -26,17 +23,21 @@ done
 
 
 # align reads with hisat2
+hisat2-build refSeq1.fna hs2_ecoli_idx
 for i in $(ls *_R1_trimmed.fastq | sed -r 's/_R1_trimmed.fastq//' | uniq)
 do
 	suffixR1="_R1_trimmed"
 	suffixR2="_R2_trimmed"
-	hisat2 -x ref_index -1 "${i}${suffixR1}.fastq" -2 "${i}${suffixR2}.fastq" -S ${i}.sam;
+	hisat2 -x hs2_ecoli_idx -1 "${i}${suffixR1}.fastq" -2 "${i}${suffixR2}.fastq" -S ${i}.sam;
 done
 
 # quantify with salmon
+# get EColi_k12_cdna.all.fa from:
+# https://ftp.ensemblgenomes.ebi.ac.uk/pub/bacteria/release-56/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655_gca_000005845/cdna/Escherichia_coli_str_k_12_substr_mg1655_gca_000005845.ASM584v2.cdna.all.fa.gz
+salmon index -t EColi_k12_cdna.all.fa -i salmon_ecoli_index
 for i in $(ls *_R1_trimmed.fastq | sed -r 's/_R1_trimmed.fastq//' | uniq)
 do
 	suffixR1="_R1_trimmed"
 	suffixR2="_R2_trimmed"
-	salmon quant -i ref_index -l A -1 "${i}${suffixR1}.fastq" -2 "${i}${suffixR2}.fastq" -o quant;
+	salmon quant -i salmon_ecoli_index -l IU -1 "${i}${suffixR1}.fastq" -2 "${i}${suffixR2}.fastq" --validateMappings -o quant;
 done 
